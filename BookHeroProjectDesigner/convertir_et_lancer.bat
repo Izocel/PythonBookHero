4e808982c572ec5d340f18b9c05f9628548f70af
RVÐ <triangle1232009@hotmail.com>
2021-10-27 22:51:44 -0400

    HotFix Initial commit

--- BookHeroProjectDesigner/convertir_et_lancer.bat
+++ BookHeroProjectDesigner/convertir_et_lancer.bat
@@ -1,7 +1,7 @@
-pyuic5 -x BookHeroProjectDesigner/ecranacceuil.ui -o Bibli_ui_designer/ecranacceuil.py
+pyuic5 -x BookHeroProjectDesigner/ecranacceuil.ui -o Application/Bibli_ui/ecranacceuil.py
 pyuic5 -x BookHeroProjectDesigner/ecranacceuil.ui -o BookHeroProjectDesigner/last_ui/ecranacceuil.py
+copy BookHeroProjectDesigner\ecranacceuil.ui Application\Bibli_ui\ecranacceuil.ui
+
 
 
 echo Succes !!!
-
-E:/Python3.9.7/python.exe "Application/main_app.py"
